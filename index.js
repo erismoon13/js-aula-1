df06ab3c6517f05209068c14b3013ac11b90d4fc
erismoon13 <erislanepereiraah44@gmail.com>
2025-07-28 17:29:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-// let numero1 = Number(prompt('Digite um número:'));
+let nome = prompt (´Digite seu nome:´);
 
-// let numero2 = Number(prompt('Digite mais um número'));
+let nota1 = Number(prompt(`Digite a primeira nota:`))
+let nota2 = Number(prompt(`Digite a segunda nota:`))
+let nota3 = Number(prompt(`Digite a terceira nota:`))
 
-// console.log(numero1 + numero2);
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log(`Olá, ${nome}! sua média é: ${media.toFixed(2)}!`)
+
+// let lado = Number(prompt(`Digite o lado do quadrado em metros:`))
+
+// let area_quadrado  = lado ** 2;
+
+// console.log(`A area do quadrado é: ${area_quadrado.toFixed(2)} metros.`)
+
+// let numero1 = Number(prompt(`Digite um número`));
+// let numero2 = Number(`Digite um número:`);
+
+// if (numero1 > numero2) {
+// console.log(`O número 1 ${número1} é maior que o número 2 ${numero2}`)
+// } else if  (numero2 > numero1) {
+// console.log (`O número 2 ${numero2} é maior que o número 1 ${numero1}`)
+// }else {
+// console.log(`Os dois números são iguais!`)
+// }
+
+if (media > = 8) {
+console.log (`o aluno ${nome},foi APROVADO com a média ${média.toFixed(2)}`);
+} else if (media > = 4) {
+console.log(`O aluno{nome},esta de RECUPERAÇÂO com média ${media.toFoxed(2)}` );
+} else {
+    console.log(`O aluno ${nome}, foi  REPROVADO com a média ${media.toFixed(2)}`);
+};
 
 
-let nome = prompt ('Digite o seu nome:')
-
-let salario = Number(prompt(`Olá, ${nome}! Agora digite seu salário`));
-
-let aumento = salario * 0.2;
-
-let novo_salario = aumento;
-
-console.log(`O seu salário que é de R$${salario.toFixed(2)} terá um  aumento de 20% ou ${aumento.toFixed(2)}, ficando: R$${novo_salario.toFixed(2)}`) 
